1725c52a54aed4db6e357f5ea5b2a9f12f43ce74
Shushanik9903 <shushanikgalstiian@gmail.com>
2025-09-12 20:25:19 +0300

    Added PickUp actores

---
+++ QuestMapPrototype/Source/QuestMapPrototype/Pickups/CoinPickUp/CoinPickup.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoinPickup.h"
+
+void ACoinPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+
+	Destroy();
+}
---
+++ QuestMapPrototype/Source/QuestMapPrototype/Pickups/EnergyPickUp/HealthPickup.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthPickup.h"
+
+
+AHealthPickup::AHealthPickup()
+{
+	bReplicates = true;
+}
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	Destroy();
+}
