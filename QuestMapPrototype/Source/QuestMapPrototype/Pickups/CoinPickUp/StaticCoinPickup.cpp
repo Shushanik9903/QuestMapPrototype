43ee180e66ce016395a6cb9014472a6b6291b1d3
Shushanik9903 <shushanikgalstiian@gmail.com>
2025-09-14 22:22:15 +0400

    Added StaticCoinPickup class

---
+++ QuestMapPrototype/Source/QuestMapPrototype/Pickups/CoinPickUp/StaticCoinPickup.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StaticCoinPickup.h"
+#include "QuestMapPrototype/GameMode/QuestGameMode.h"
+
+AStaticCoinPickup::AStaticCoinPickup()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    PickupType = EPickupType::Coin;
+}
+
+void AStaticCoinPickup::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void AStaticCoinPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+
+    if (AQuestGameMode* GM = Cast<AQuestGameMode>(GetWorld()->GetAuthGameMode()))
+    {
+        int32 NewCoinsCollected = GM->GetCoinsCollected() + 1;
+        int32 CoinsNeededForShield = GM->GetCoinsThreshold();
+
+        GM->SetCoinsCollected(NewCoinsCollected);
+
+
+        if (NewCoinsCollected >= CoinsNeededForShield)
+        {
+            GM->OnShieldSpawn.Broadcast(GetActorLocation());
+        }
+    }
+}
---
+++ QuestMapPrototype/Source/QuestMapPrototype/Pickups/CoinPickUp/StaticCoinPickup.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../Pickup.h"
+#include "StaticCoinPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUESTMAPPROTOTYPE_API AStaticCoinPickup : public APickup
+{
+	GENERATED_BODY()
+
+
+public:
+	AStaticCoinPickup();
+protected:
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	) override;
+
+
+protected:
+	virtual void BeginPlay() override;
+	
+};
